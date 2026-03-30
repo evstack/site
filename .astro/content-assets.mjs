dcfcdf74acdb8896a92c17e4a554cd300b7c7f63
tac0turtle <marko@baricevic.me>
2026-03-30 13:04:49 +0200

    fix(remove dist): remove dist

--- .astro/content-assets.mjs
+++
@@ -1,4 +0,0 @@
-
-import __ASTRO_IMAGE_IMPORT_WfptY from "figures/header_shares_commit.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fadr%2Fadr-007-header-commit-to-shares.md";
-export default new Map([["figures/header_shares_commit.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fadr%2Fadr-007-header-commit-to-shares.md", __ASTRO_IMAGE_IMPORT_WfptY]]);
-		
